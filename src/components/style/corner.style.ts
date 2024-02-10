6a054e3808193e751e8d73d2030a2737ebc4e0fa
akash02ab <akash02.ab@gmail.com>
2024-02-10 14:10:58 +0530

    feat: add corner component

---
+++ src/components/style/corner.style.ts
@@ -0,0 +1,21 @@
+const githubCorner = `
+  absolute
+  top-0
+  right-0
+  border-0
+  fill-blue
+  text-off-white
+  group
+`;
+
+const octoArm = `
+  origin-[130px_106px]
+  group-hover:animate-wave
+`;
+
+const style = {
+  githubCorner,
+  octoArm
+};
+
+export default style;
