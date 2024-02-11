a295fbf81bb3a120176889de2de211848bc66ef7
akash02ab <akash02.ab@gmail.com>
2024-02-11 21:01:26 +0530

    feat: add repository component

---
+++ src/components/Repository.tsx
@@ -0,0 +1,112 @@
+import { useEffect, useState } from "react";
+import DropDown from "./Dropdown";
+import Icon from "./Icons";
+import langColors from "@/utils/langColors";
+import style from "./style/repository.style";
+
+type Repo = {
+  description: string;
+  fork: boolean;
+  forks: number;
+  forks_count: number;
+  html_url: string;
+  id: number;
+  language: string;
+  name: string;
+  stargazers_count: number;
+  size: number;
+};
+
+interface PropType {
+  repos: Repo[]
+};
+
+const sortTypes: string[] = ['stars', 'forks', 'size'];
+
+type Map = Record<typeof sortTypes[number], keyof Repo>;
+
+export default function Repository({ repos }: PropType) {
+  const [sortType, setSortType] = useState<typeof sortTypes[number]>(sortTypes[0]);
+  const [topRepos, setTopRepos] = useState<Repo[]>([]);
+
+  const getTopRepos = (type: keyof typeof map) => {
+    const LIMIT = 8;
+    const map: Map = {
+      stars: 'stargazers_count',
+      forks: 'forks_count',
+      size: 'size',
+    };
+    const sortProperty = map[type];
+    const sorted = repos
+      .filter(repo => !repo.fork)
+      .sort((a, b) => (b[sortProperty] as number) - (a[sortProperty] as number))
+      .slice(0, LIMIT);
+
+    setTopRepos(sorted);
+  }
+
+  useEffect(() => {
+    if (repos.length) getTopRepos(sortType);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [sortType])
+
+  return (
+    <section className={style.repoSection}>
+      <header className={style.repoHeader}>
+        <h2 className={style.repoTitle}>Top Repos</h2>
+        <span className={style.repoSubtext}>by</span>
+        <DropDown
+          items={sortTypes}
+          value={sortType}
+          onValueChange={setSortType}
+        />
+      </header>
+      <div>
+        {topRepos.length ? (
+          <ul className={style.repos}>
+            {topRepos.map((repo) => {
+              const language = repo.language || 'Markdown';
+              return (
+                <li key={repo.id}>
+                  <a href={repo.html_url} target="_blank" className={style.repo}>
+                    <div className={style.repoTop}>
+                      <div className={style.repoHead}>
+                        <Icon name="repository" className="w-6 h-6" />
+                        <h3 className={style.repoName}>{repo.name}</h3>
+                      </div>
+                      <p className={style.repoDesc}>{repo.description}</p>
+                    </div>
+                    <div className={style.repoBottom}>
+                      <div className={style.repoLeft}>
+                        <p className={style.repoStat}>
+                          <i
+                            style={{ backgroundColor: langColors[language] }}
+                            className={style.langCircle}
+                          />
+                          <span>{language}</span>
+                        </p>
+                        <p className={style.repoStat}>
+                          <Icon name="star" className="w-4 h-4" />
+                          <span>{repo.stargazers_count.toLocaleString()}</span>
+                        </p>
+                        <p className={style.repoStat}>
+                          <Icon name="fork" className="w-4 h-4" />
+                          <span>{repo.forks.toLocaleString()}</span>
+                        </p>
+                      </div>
+                      <div className="right">
+                        <p className={style.repoStat}>{repo.size.toLocaleString()} KB</p>
+                      </div>
+                    </div>
+                  </a>
+                </li>
+              )
+            })}
+          </ul>
+        ) : (
+            <p>No available repositories!</p>
+        )}
+      </div>
+    </section>
+  )
+}
---
+++ src/components/style/repository.style.ts
@@ -0,0 +1,135 @@
+const repoSection = `
+  p-4
+  xxs:p-8
+  xs:p-16
+  sm:p-24
+  max-w-7xl
+  flex
+  flex-col
+  space-y-12
+`;
+
+const repoHeader = `
+  flex
+  items-end
+  space-x-4
+`;
+
+const repoTitle = `
+  text-xl
+  xxs:text-2xl
+  xs:text-3xl
+  sm:text-4xl
+  text-off-white
+  font-bold
+  underline
+  decoration-2
+  decoration-dashed
+  decoration-grey
+`;
+
+const repoSubtext = `
+  text-xs
+  xxs:text-sm
+  xs:text-base
+  sm:text-lg
+  text-grey
+`;
+
+const repos = `
+  grid
+  grid-cols-[repeat(auto-fit,minmax(300px,1fr))]
+  gap-5
+`;
+
+const repo = `
+  h-full
+  p-4
+  xs:p-8
+  flex
+  flex-col
+  justify-between
+  space-y-8
+  bg-dark-grey
+  text-white
+  rounded
+`;
+
+const repoTop = `
+  flex
+  flex-col
+  space-y-2
+`;
+
+const repoHead = `
+  flex
+  items-center
+  space-x-2
+`;
+
+const repoName = `
+  font-semibold
+  text-off-white
+  text-sm
+  xxs:text-base
+  xs:text-lg
+  sm:text-xl
+  text-ellipsis
+  whitespace-nowrap
+  overflow-hidden
+`;
+
+const repoDesc = `
+  font-light
+  text-xs
+  xxs:text-sm
+  xs:text-base
+  text-lightest-blue
+`;
+
+const repoBottom = `
+  flex
+  flex-wrap
+  items-center
+  justify-between
+`;
+
+const repoLeft = `
+  flex
+  items-center
+  space-x-3
+`;
+
+const langCircle = `
+  inline-block
+  w-3
+  h-3
+  rounded-full
+`;
+
+const repoStat = `
+  flex
+  items-center
+  space-x-1
+  text-xs
+  text-slate
+`;
+
+const style = {
+  repoSection,
+  repoHeader,
+  repoTitle,
+  repoSubtext,
+  repos,
+  repo,
+  repoTop,
+  repoHead,
+  repoName,
+  repoDesc,
+  repoBottom,
+  repoLeft,
+  langCircle,
+  repoStat
+};
+
+export default style;
